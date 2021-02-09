3679fa89ebf211ad38d3dae76bfd5c3c172a88bb
Johnny Samoletov <johnny_samoletov@amp.com.au>
2021-02-10 09:00:24 +1100

    feat: implementation of getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,14 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const responses = await Promise.all(urls.map((url) => httpGet(url)));
+  return responses.map((response) =>
+    response.status === 200
+      ? {
+          'Arnie Quote': JSON.parse(response.body).message,
+        }
+      : { FAILURE: 'Your request has been terminated' }
+  );
 };
 
 module.exports = {
